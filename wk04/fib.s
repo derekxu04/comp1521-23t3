059b244df06b4d7b42665512ff09641744996228
Derek Xu <xuderek2004@gmail.com>
2023-10-03 15:17:31 +1100

    week 4 answers

--- wk04/fib.s
+++ wk04/fib.s
@@ -1,9 +1,12 @@
-# A simple, recursive, non-memoized fibonacci implementation.
-
 main:
+
+main__prologue:
+	begin
+	push	$ra
 
 main__body:
 
+	# $t0 = n
 	li	$v0, 4			# syscall 4: print_string
 	la	$a0, enter_number_msg	#
 	syscall				# printf("Enter a number: ");
@@ -25,12 +28,22 @@ main__body:
 	la	$a0, fib_msg_2		#
 	syscall				# printf(" is ");
 
-	# TODO: print fib(n)
+	move	$a0, $t0		# n into $a0
+	jal	fib
+	move	$t2, $v0		# result of fib(n)
+
+	li	$v0, 1			# syscall 1: print_int
+	move	$a0, $t2		#
+	syscall				# #printf("%d", fib(n));
 
 
 	li	$v0, 11			# syscall 11: print_char
 	li	$a0, '\n'		#
 	syscall				# putchar('\n');
+
+main__epilogue:
+	pop	$ra
+	end
 
 	li	$v0, 0
 	jr	$ra				# return 0;
@@ -42,22 +55,72 @@ main__body:
 fib:
 
 	# Args: 
-	# - 
+	# - $a0 - int: n
 	# Returns: 
-	# - 
+	# - int - result
 	#
-	# Frame:	[]
-	# Uses: 	[]
-	# Clobbers:	[]
+	# Frame:	[$ra, $s0, $s1, $s2]
+	# Uses: 	[$v0, $a0, $t0, $s0, $s1, $s2]
+	# Clobbers:	[$v0, $a0, $t0]
 	#
 	# Locals:
-	#   - 
+	#   - $s0: int n
+	#   - $t0: int return value
 	#
 	# Structure:
-	#   - 
+	#   - fib
+	#     -> [prologue]
+	#     -> [body]
+	#	-> [fib__n_ge_2]
+	#	-> [fib__epi]
+	#     -> [epilogue]
+
+fib__prologue:
+	begin
+	push	$ra
+	push	$s0
+	push	$s1
+	push	$s2
+
 
 fib__body:
-	# TODO
+	move	$s0, $a0	# $s0 contains n now
+	bge	$s0, 2, fib__n_ge_2	#if (n >= 2) goto fib__n_ge_2;
+
+	move	$t0, $s0	#retval = n;
+	j 	fib__epi 		#goto fib__epi;
+    
+
+fib__n_ge_2:
+#retval = fib(n - 1) + fib(n - 2);
+
+	# call fib(n - 1)
+	sub	$a0, $s0, 1	# argument = n-1 
+	jal	fib
+	move	$s1, $v0	# return value
+
+	# call fib(n-2)
+	sub	$a0, $s0, 2	# $argument = n-2 
+	jal	fib
+	move	$s2, $v0	# return value
+
+	# add both to get the result
+	add	$t0, $s1, $s2
+
+fib__epi:
+    #return retval;
+	move	$v0, $t0
+
+
+
+fib__epilogue:
+	pop	$s2
+	pop	$s1
+	pop	$s0
+	pop	$ra
+	end
+
+	jr	$ra				# return 0;
 
 
 .data
--- wk04/q2.s
+++ wk04/q2.s
@@ -11,20 +11,51 @@
 FLAG_ROWS = 6
 FLAG_COLS = 12
 
+# Registers:
+# - row in $t0
+# - col in $t1
+# - row offset in $t2
+# - row+col offset in $t3
 main:
+main__row_loop_init:                    # for (int row = 0; row < FLAG_ROWS; row++) {
+	li	$t0, 0                  # row = 0
 
-row_loop_init:
-row_loop_cond:
-row_loop_body:
+	main__row_loop_body:            # while (row < FLAG_ROWS)
+	bge	$t0, FLAG_ROWS, main__row_loop_end
 
-col_loop_init:
-col_loop_cond:
-col_loop_body:
-col_loop_end:
+	main__col_loop_init:            # for (int col = 0; col < FLAG_COLS; col++) {
+	li	$t1, 0                  # col = 0;
 
-row_loop_end:
+	main__col_loop_body:            # while (col < FLAG_COLS)
+	bge	$t1, FLAG_COLS, main__col_loop_end
 
-epilogue:				# End of the program.
+					# convert [row][col] to byte offset
+	mul	$t2, $t0, FLAG_COLS     # 1) row offset = rowNumber * NUM_COLUMNS
+	add	$t3, $t2, $t1           # 2) row+col offset = rowOffset + colNumber
+					# 3) byte offset = row+col offset * sizeof(char)
+					#    (Step 3 is skipped since sizeof(char) == 1)
+	la	$t4, flag               # 4) Add the base address of the flag array to the byte offset.
+	add	$t5, $t3, $t4           #    This produces the complete memory address of the flag element.
+
+	lb	$a0, ($t5)              # 5) Load the byte at the byte offset into $a0
+
+	li	$v0, 11                 # printf("%c", flag[row][col])
+	syscall
+
+	main__col_loop_iter:
+	addi	$t1, $t1, 1             # col++
+	j	main__col_loop_body
+
+	main__col_loop_end:
+	main__row_loop_iter:
+	li	$a0, '\n'               # printf("%c", '\n')
+	li	$v0, 11
+	syscall
+	addi	$t0, $t0, 1             # row++
+	j	main__row_loop_body
+
+	main__row_loop_end:
+					# End of the program.
 	li	$v0, 0                  # return 0
 	jr	$ra
 
