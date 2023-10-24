dde20aeec68cf50378f004356cec5cb0fa8f428b
Derek Xu <xuderek2004@gmail.com>
2023-10-24 15:05:22 +1100

    wk07 code

---
+++ wk07/six_middle_bits.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void print_word_binary(char *string, uint32_t value);
+
+/* 
+For example:
+00000000 00000101 01000000 01110110 
+
+middle six bits are 10 1010
+*/
+
+// TODO
+uint32_t six_middle_bits(uint32_t u) {
+    u >>= 13;
+    return u & 0x3F;
+}
+
+int main(void) {
+    // num = 0x54076
+    uint32_t num = 0b00000000000001010100000001110110;
+
+    // We want the middle 0x2A (0b00101010)
+    uint32_t result = six_middle_bits(num);
+    printf("Middle: 0x%X\n", result);
+
+    return 0;
+}
+
+////////////// HELPER FUNCTIONS //////////////
+void print_word_binary(char *string, uint32_t value) {
+    printf("%s\n", string);
+    for (int i = 31; i >= 0; i--) {
+        putchar(((value & ((uint32_t)0x1 << i)) >> i) + 48);
+    }
+    putchar('\n');
+}
